4b29530429d41d94fb83fcad24cbf881e3bfc14a
JoelMoukoko <joelpatrickmoukoko03@gmail.com>
2024-05-16 16:42:42 +0200

    last one

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY =''
     AZURE_SUBSCRIPTION_KEY = '81f1a4bc58cd455aa975842b8a6fc0d5'
     AZURE_ENDPOINT = 'https://vision-finall.cognitiveservices.azure.com/'
-    OPENAI_API_KEY = 'sk-proj-C6joKkEq0v0OFGZJyb2aT3BlbkFJr1yyhwMpXH7RCAPKzc0b'
+    OPENAI_API_KEY = ''
