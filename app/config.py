fff71248898a333dde075ee60d3a18fc9d0f0f23
JoelMoukoko <joelpatrickmoukoko03@gmail.com>
2024-05-16 16:34:21 +0200

    dernier

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY =
+    SECRET_KEY =''
     AZURE_SUBSCRIPTION_KEY = '81f1a4bc58cd455aa975842b8a6fc0d5'
     AZURE_ENDPOINT = 'https://vision-finall.cognitiveservices.azure.com/'
     OPENAI_API_KEY = 'sk-proj-C6joKkEq0v0OFGZJyb2aT3BlbkFJr1yyhwMpXH7RCAPKzc0b'
